e116002a4cf882ef67a66f360749e73dc30d54ce
Harshit Raghuwanshi <harshuraghu7999@gmail.com>
2026-05-28 14:28:34 +0530

    feat: implement note creation endpoint with validation

--- src/app.js
+++ src/app.js
@@ -1,15 +1,43 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import express from 'express';
+import express, { Router } from 'express';
 import connectDB from './config/db.js';
+import NoteModel from './models/note.model.js';
 
 connectDB();
 
+app.use(express.json());
 const app = express();
  
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// @route Post /api/post
+// @desc Create a new post
+// @access Public
+
+
+app.post("/api/post", async (req, res) => {
+const { title, description } = req.body;
+
+
+// Validation
+if(!description){
+    return res.status(400).json({ message: "Description is required" });
+}
+
+if(title.trim().length < 4 ){
+    return res.status(400).json({ message: "Title must be at least 4 characters long" });
+}
+
+if(description.trim().length < 10 ){
+    return res.status(400).json({ message: "Description must be at least 10 characters long" });
+}
+
+// if validation passes, create a new note 
+
+let newNote =  await NoteModel.create({ title, description });
+
+
+    res.json({ message: "Note created successfully", note: newNote });
 });
 
 export default app;
